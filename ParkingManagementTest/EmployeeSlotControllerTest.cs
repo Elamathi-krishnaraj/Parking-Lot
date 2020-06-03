7303e5c14fe4a628c688e57c76898db3e54a519c
Elamathi-krishnaraj <krishelamathi@gmail.com>
2020-06-03 18:27:53 +0530

    added unit test project and validation to login

---
+++ ParkingManagementTest/EmployeeSlotControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using ParkingManagement.Controllers.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Web;
+using System;
+
+namespace ParkingManagementTest
+{
+    [TestFixture]
+    public class EmployeeSlotControllerTest
+    {
+        [Test]
+        public void GetEmployeeSlot_IdIsZero_ReturnNotFound()
+        {
+            var controller = new EmployeeSlotController();
+
+            //var result = controller.GetEmployeeSlot(0);
+
+            //// NotFound 
+            //Assert.That(result, Is.TypeOf<NotFound>());
+
+        }
+    }
+}
---
+++ ParkingManagementTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ParkingManagementTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ParkingManagementTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79e66ea0-a95f-4265-b349-871df5b47de6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
